2f7d75260e03e71941ab5b9c5c39eafe40688513
karma-2day <ashar.ahmed7307@hhs.edu.pk>
2023-07-18 18:27:29 +0500

    hello world

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,20 @@ import Head from 'next/head'
 import Script from 'next/script'
 import Navbar from '@/components/navbar'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  console.log(router.pathname)
   return <> 
   <Head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
   </Head>
   <Script         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"/>
         
-        <Navbar {...pageProps}/>
-  <Component {...pageProps} /></>
+      {router.pathname=='/'||router.pathname=="/category/[slug]" &&  <Navbar {...pageProps}/>}
+  <Component {...pageProps} />
+  
+  </>
 }
 
 
